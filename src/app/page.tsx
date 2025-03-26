11683384250e5a4152ac61ec1b17b12b04818acc
Ekata1233 <jadhavjanki8462@gmail.com>
2025-03-26 13:12:49 +0530

    Updated home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./componants/Hero";
+import OfficeTour from "./componants/OfficeTour/page";
 
 export default function Home() {
   return (
     <main className="mt-16">
       <Hero/>
+      <OfficeTour/>
       <h1>kam kr</h1>
       <h2>gp</h2>
     </main>
