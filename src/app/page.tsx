1d7b86e1aee5ba18bd9d90f8b121537ca43250ea
aniket-patil7075 <akp7075@gmail.com>
2025-03-26 12:49:42 +0530

    Explore office done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import ExploreOffice from "./componants/exploreOffice/page";
 import Hero from "./componants/Hero";
+
 
 export default function Home() {
   return (
     <main className="mt-16">
       <Hero/>
-      <h1>kam kr</h1>
-      <h2>gp</h2>
+      <ExploreOffice />
     </main>
   );
 }
