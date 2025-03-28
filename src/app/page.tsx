594be6a06795f1da6730d985b92973386323be22
Ekata1233 <jadhavjanki8462@gmail.com>
2025-03-28 11:40:08 +0530

    update the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <Hero/>
       <Offices />
       <OfficeTour/>  
-      <WorkBusiness />    
+      {/* <WorkBusiness />    
       <SecondWorkBusiness />
-      <ThirdWorkBusiness />
+      <ThirdWorkBusiness /> */}
       <ExploreOffice />
       <OurAmenites />
       
