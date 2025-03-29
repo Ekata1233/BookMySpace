d4e0122350728cd44cfe31ab67e5d4e9c133db0a
aniket-patil7075 <akp7075@gmail.com>
2025-03-29 14:22:17 +0530

    office details done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,20 @@ import SecondWorkBusiness from "./componants/secondWorkBusiness/page";
 import ThirdWorkBusiness from "./componants/thirdWorkBusiness/page";
 import Offices from "./componants/offices/page";
 import Boxes from "./componants/boxes/page";
+import OfficeDetails from "./componants/officeDetails/page";
 
 
 export default function Home() {
   return (
     <main className="mt-16">
       <Hero/>
+      <OfficeDetails />
       <Offices />
       <OfficeTour/>  
       <WorkBusiness />    
       <SecondWorkBusiness />
       <ThirdWorkBusiness />
       <ExploreOffice />
-      <OurAmenites />
       <OurAmenites /> 
       
     </main>
